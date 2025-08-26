75dd5b1dffa1590f6199a1768d74326b6b1bc48e
thalesantana <thales.c.santana@outlook.com>
2025-08-26 15:45:48 -0300

    feat: remove Section2 component to streamline Home page layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,10 @@
 import HeroSection from "@/components/hero/hero-section";
 import Launchpad from "@/components/launchpad/launchpad";
-import Section2 from "@/components/section2/section2";
 
 export default function Home() {
   return (
     <div className="flex flex-col relative flex-1 w-full items-center overflow-hidden">
       <HeroSection />
-      {/* <Section2 /> */}
       <Launchpad />
     </div>
   );
